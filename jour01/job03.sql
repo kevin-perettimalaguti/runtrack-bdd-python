de43d7c4ef45acc47641caa558ec5fddd49a6d2f
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-01-29 13:49:47 +0100

    push final avec rectification minim

--- jour01/job03.sql
+++ jour01/job03.sql
@@ -1,7 +1,7 @@
 -- Active: 1706528093603@@127.0.0.1@3306@laplateforme
 USE LaPlateforme;
 
-CREATE TABLE Etudiant (
+CREATE TABLE etudiants (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
     prenom VARCHAR(255) NOT NULL,
--- jour01/job05.sql
+++ jour01/job05.sql
@@ -1,5 +1,5 @@
 -- Active: 1706528093603@@127.0.0.1@3306@laplateforme
-INSERT INTO etudiant (nom,prenom,age,email) VALUES 
+INSERT INTO etudiants (nom,prenom,age,email) VALUES 
 ('Steak','Chuck',45,'chuck.steak@laplateforme.io'),
 ('Doe','John',18,'john,doe@laplateforme.io'),
 ('Barnes','Binkie',16,'binkie.barnes@laplateforme.io'),
