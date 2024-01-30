8504e56d1b61f4c682685e3beffe2b62d379fcf4
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-01-30 11:03:20 +0100

    job 05

--- jour02/job05.py
+++ jour02/job05.py
@@ -6,3 +6,15 @@ mydb = mysql.connector.connect(
     password = "1478",
     database = "Laplateforme",
 )
+
+cursor = mydb.cursor()
+cursor.execute("SELECT superficie FROM etage")
+superficie = cursor.fetchall()
+print(superficie)
+result = 0
+
+for i in superficie:
+    result += i[0]
+
+print(f"La superficie de La Plateforme est de {result} m2")
+
